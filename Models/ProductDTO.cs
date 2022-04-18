ec2a83d2ddfb4b1b7faee9001fd91db31c557bf5
Aiden Bridger <bridger.aiden@gmail.com>
2022-04-17 20:32:11 -0600

    Add project files.

---
+++ Models/ProductDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace April17Assignment2.Models
+{
+    public class ProductDTO
+    {
+        public string ProductName { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public decimal? UnitePrice { get; set; }
+    }
+}
